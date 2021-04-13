947a75e34946ca7e313cb2f911a0957cb25aeb0e
Jasper Alani <jasperalani@gmail.com>
2021-04-13 23:43:05 +0100

    Adds function to connect database

---
+++ src/models/setup.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/house-mates/api/src/models/models"
+)
+
+var DB *gorm.DB
+
+func ConnectDataBase() {
+
+	dsn := "root:rootpass@tcp(housemates-mariadb:3306)/dbname?charset=latin1&parseTime=True&loc=Local"
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.AutoMigrate(
+		&models.User{},
+		&models.Usermeta{},
+		&models.Action{},
+		&models.Event{},
+		&models.Response{},
+	)
+
+	DB = database
+}
